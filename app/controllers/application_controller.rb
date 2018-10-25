4bbf59ec789a3bd3e4237d3c4b514a33f5666aad
Denis Oster <denis.oster.dev@gmail.com>
2018-10-25 21:28:45 +0300

    Configured ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { detail: exception }, status: :unauthorized
+  end
 end
