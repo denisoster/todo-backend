3d7e037efc582ee6f07a0d1c86a110e0c036f161
Denis Oster <denis.oster.dev@gmail.com>
2018-10-25 21:25:05 +0300

    Implement fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
